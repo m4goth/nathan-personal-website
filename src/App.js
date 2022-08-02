a378546b3827b8e77b8ee2433c95bac90f9fdf52
m4goth <matheusdealmeidasilveira@gmail.com>
2022-08-02 18:55:40 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ResponsiveAppBar from './components/app-bar';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar/>
+    </div>
+  );
+}
+
+export default App;
