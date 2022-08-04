23fc992a58ed0493c3b7a4bab488b31807ed748b
m4goth <matheusdealmeidasilveira@gmail.com>
2022-08-04 17:37:24 -0300

    projeto refatorado, adicionado imagem png na pasta, organizado pastas e componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ResponsiveAppBar from './components/app-bar';
+import Home from './components/home/home';
+import grade from './components/pages/pag-inicial';
+import contatobutton from './components/contato-button';
 
 function App() {
   return (
     <div className="App">
-      <ResponsiveAppBar/>
+      <Home/>
+      <grade/>
+      <contatobutton/>
     </div>
   );
 }
---
+++ src/components/contato-button.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+const contatobutton = () => {
+    // function BasicButtons() {
+        return (
+            <Stack spacing={2} direction="row">
+            <Button variant="contained">Entre em contato</Button>
+            </Stack>
+        );
+    // };
+};
+export default contatobutton;
