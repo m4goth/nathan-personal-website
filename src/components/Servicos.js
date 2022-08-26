293fa8bb3c5b34eb8958c9d2701762871b7ad441
m4goth <matheusdealmeidasilveira@gmail.com>
2022-08-26 17:56:09 -0300

    montado a estrutura de grid para a página Serviços

--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -1,3 +1,48 @@
-export function Servicos(){
-    return <h2>Serviços</h2>
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
+import Grid from '@mui/material/Grid';
+
+
+export function Servicos() {
+  return (
+        <Container maxWidth="sm">
+            <h1 className='tittle'>Serviços</h1>
+            <Grid container spacing={2}>
+                {/* serviço 1 */}
+                <Grid item xs={8}>
+                    <h1>img</h1>
+                </Grid>
+                <Grid item xs={4}>
+                    <h2>text</h2>
+                    <button>Ver mais</button>
+                </Grid>
+                
+                <br></br>
+                {/* serviço 2 */}
+                <Grid item xs={4}>
+                    <h1>text</h1>
+                    <button>Ver mais</button>
+                </Grid>
+                <Grid item xs={8}>
+                    <h2>img</h2>
+                </Grid>
+
+                <br></br>
+                {/* serviço 3 */}
+                <Grid item xs={8}>
+                    <h1>img</h1>
+                </Grid>
+                <Grid item xs={4}>
+                    <h2>text</h2>
+                    <button>Ver mais</button>
+                </Grid>
+            </Grid>
+        </Container>
+  );
 }
+
+
+// export function Servicos(){
+//     return <h2>Serviços</h2>
+// }
