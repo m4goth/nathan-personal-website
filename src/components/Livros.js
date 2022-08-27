aaa3eda696395e98cc79b4053de833d6a94cdaf7
m4goth <matheusdealmeidasilveira@gmail.com>
2022-08-27 16:24:20 -0300

    criado a estrutura de Livros, refatorado estrutura Servicos

--- src/components/Livros.js
+++ src/components/Livros.js
@@ -1,3 +1,39 @@
-export function Livros() {
-    return <h2>Livros</h2>
-}
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
+import '../css/Livros.css';
+
+const Livros = () => {
+    return (
+      <Container className="container">
+        <Grid container spacing={2} sx={{p:4}}>
+          <Grid xs={12} sm={6} className="grid-left">
+            <h1>Livros</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac egestas facilisi arcu vivamus. Hac egestas facilisi arcu vivamus. Sit magna at id ipsum sit id viverra felis et. Pellentesque amet tristique rhoncu, donec a euismod velit. Hendrerit molestie fermentum odi euismod.</p>
+          </Grid>
+
+          <Grid xs={12} sm={6} className="grid-right">
+            <Grid container spacing={2} sx={{p:4}}>
+                <Grid xs={8} sm={6}>
+                    <svg width="" height="" viewBox="300 100 664 614" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="664" height="614" fill="#C2C2C2"/>
+                    </svg>               
+                </Grid>
+                <Grid xs={8} sm={6}>
+                    <svg width="" height="" viewBox="300 100 664 614" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="664" height="614" fill="#C2C2C2"/>
+                    </svg>    
+                </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  };
+  
+  export { Livros };
+
+// export function Livros() {
+//     return <h2>Livros</h2>
+// }
--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -3,6 +3,7 @@ import Container from '@mui/material/Container';
 
 import Grid from '@mui/material/Grid';
 
+import Button from '@mui/material/Button';
 
 export function Servicos() {
   return (
@@ -10,32 +11,45 @@ export function Servicos() {
             <h1 className='tittle'>Serviços</h1>
             <Grid container spacing={2}>
                 {/* serviço 1 */}
-                <Grid item xs={8}>
-                    <h1>img</h1>
+                <Grid item xs={8} sm={6}>
+                    {/* <h1>img</h1> */}
+                    <svg width="" height="" viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="664" height="614" fill="#0E5B83"/>
+                    </svg>
+
                 </Grid>
-                <Grid item xs={4}>
-                    <h2>text</h2>
-                    <button>Ver mais</button>
+                <Grid item xs={4} sm={6} className="grid">
+                    <h2>Cursos</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac egestas facilisi arcu vivamus.</p>
+                    <Button variant="outlined">Ver mais</Button>
                 </Grid>
                 
                 <br></br>
                 {/* serviço 2 */}
-                <Grid item xs={4}>
-                    <h1>text</h1>
-                    <button>Ver mais</button>
+                <Grid item xs={4} sm={6}>
+                    <h2>Consultoria</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac egestas facilisi arcu vivamus.</p>
+                    <Button variant="outlined">Ver mais</Button>
                 </Grid>
-                <Grid item xs={8}>
-                    <h2>img</h2>
+                <Grid item xs={8} sm={6}>
+                    {/* <h1>img</h1> */}
+                    <svg width="" height="" viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="664" height="614" fill="#0E5B83"/>
+                    </svg>
                 </Grid>
 
                 <br></br>
                 {/* serviço 3 */}
-                <Grid item xs={8}>
-                    <h1>img</h1>
+                <Grid item xs={8} sm={6}>
+                    {/* <h1>img</h1> */}
+                    <svg width="" height="" viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="664" height="614" fill="#0E5B83"/>
+                    </svg>
                 </Grid>
-                <Grid item xs={4}>
-                    <h2>text</h2>
-                    <button>Ver mais</button>
+                <Grid item xs={4} sm={6}>
+                    <h2>Palestras</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac egestas facilisi arcu vivamus.</p>
+                    <Button variant="outlined">Ver mais</Button>
                 </Grid>
             </Grid>
         </Container>
