333413d8db7e4fa0810bb8b2d0a23bf2d0fbf1e7
m4goth <matheusdealmeidasilvira@gmail.com>
2022-09-05 23:19:13 -0300

    trocado svg por box

--- src/components/Servicos.js
+++ src/components/Servicos.js
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
 
 export function Servicos() {
   return (
@@ -13,10 +14,7 @@ export function Servicos() {
                 {/* serviço 1 */}
                 <Grid item xs={8} sm={6}>
                     {/* <h1>img</h1> */}
-                    <svg  viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="664" height="614" fill="#0E5B83"/>
-                    </svg>
-
+                    <Box sx={{backgroundColor: "primary.main", width: "50%", height:"450px", margin: "0 auto"}} />
                 </Grid>
                 <Grid item xs={4} sm={6} className="grid">
                     <h2>Cursos</h2>
@@ -33,19 +31,15 @@ export function Servicos() {
                 </Grid>
                 <Grid item xs={8} sm={6}>
                     {/* <h1>img</h1> */}
-                    <svg  viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="664" height="614" fill="#0E5B83"/>
-                    </svg>
-                </Grid>
+                    <Box sx={{backgroundColor: "primary.main", width: "50%", height:"450px", margin: "0 auto"}} />
+                   </Grid>
 
                 <br></br>
                 {/* serviço 3 */}
                 <Grid item xs={8} sm={6}>
                     {/* <h1>img</h1> */}
-                    <svg viewBox="300 100 664 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="664" height="614" fill="#0E5B83"/>
-                    </svg>
-                </Grid>
+                    <Box sx={{backgroundColor: "primary.main", width: "50%", height:"450px", margin: "0 auto"}} />
+                  </Grid>
                 <Grid item xs={4} sm={6}>
                     <h2>Palestras</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac egestas facilisi arcu vivamus.</p>
