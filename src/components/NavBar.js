76a1fe1c439bf792f7e61b11c86ea51387fd13b0
m4goth <matheusdealmeidasilveira@gmail.com>
2022-10-26 12:40:38 -0300

    ajustado foto

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -105,7 +105,7 @@ export function NavBar() {
                 textDecoration: 'none',
               }}
             >
-              <img src={Logo} alt="logo"></img> 
+              <img src={Logo} alt="Logo"/> 
             </Typography>
 
             {/* estático */}
